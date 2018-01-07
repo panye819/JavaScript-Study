14d0d289fc937d9a6d69b2e6344f08979f34f35a
castlepan <panye819@gmail.com>
2018-01-08 00:10:10 +0800

    add nodejs async

--- 07-nodejs/02-async-sync.js
+++ 07-nodejs/02-async-sync.js
@@ -3,10 +3,12 @@ const fs=require('fs');
 file='test.txt';
 console.log('开始读取文件...');
 
+//同步读取文件
 // data=fs.readFileSync(file); 
+// console.log(data.toString());
+
+//异步读取文件
 data=fs.readFile(file,function (err,data) {
     console.log(data.toString());
 });
-// console.log(data.toString());
-
 console.log('读取文件完毕...');
