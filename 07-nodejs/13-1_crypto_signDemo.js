331f983fa228d322de4f8e536744029103d19749
panye819 <panye819@gmail.com>
2018-03-08 09:44:56 +0800

    add crypto

---
+++ 07-nodejs/13-1_crypto_signDemo.js
@@ -0,0 +1,7 @@
+var crypto = require('crypto');
+var fs = require('fs');
+var pem = fs.readFileSync('key.pem');
+var key = pem.toString('ascii');
+var sign = crypto.createSign('RSA-SHA256');
+sign.update('test');
+console.log(sign.sign(key,'hex'));
