a357d00388420e857ae88cb3ca54fd2970b8760a
panye <panye819@gmail.com>
2020-01-14 15:25:24 +0800

    refactor

--- Blog2/routers/admin.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-router.get('/user',function (req,res,next) {
-    res.send('Admin-User');
-});
-
-module.exports = router;
--- nodejs-miaowei/01-study/1.js
+++
@@ -1,4 +0,0 @@
-var m1 = require('./2');//这个方法的返回值就是被加载模块中的module.exports
-
-console.log(m1);
-
